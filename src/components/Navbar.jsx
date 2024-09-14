41e9706794b6dfce6b25d1f28c62e729aceb70fa
Ritika Ghosh <ritikaghoshriti@gmail.com>
2024-09-14 23:07:45 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,23 @@ import Avatar from 'react-avatar';
 function Navbar() {
   return (
     <div className='flex justify-between px-6 py-2'>
-        <div className='flex items-center space-x-4 border border-black'><AiOutlineMenu className='text-xl' /><img src={youtube} alt='' className='w-24 cursor-pointer'></img> </div>
-        <div className='flex w-[40%] border border-black'><input type ='text'placeholder='Search' className='outline-none'/><CiSearch /><IoMdMic /></div>
-        <div className='border border-black'>
-          <Avatar src={profile} size='32' round={true}/>
+        <div className='flex items-center space-x-4 border border-black'>
+          <AiOutlineMenu className='text-xl cursor-pointer' />
+          <img src={youtube} alt='' className='w-24 cursor-pointer'></img> 
+          </div>
+        <div className='flex w-[40%] border border-black'>
+          <div className='w-[100%] px-3 py-2 border rounded-l-full' >
+          <input type ='text'placeholder='Search' className='outline-none'/>
+          </div>
+          <button className='px-4 py-2 border bg-gray-200 rounded-r-full'>
+          <CiSearch size={'24px'}/>
+          </button>
+          <button>
+          <IoMdMic size={'42px'} className='ml-3 border rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200 '/>
+          </button>
+          </div>
+        <div className='flex space-x-5 items-center border border-black'>
+          <Avatar src={profile} size='42' round={true}/>
         </div>
     </div>
   )
