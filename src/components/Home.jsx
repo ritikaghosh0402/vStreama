67ea238bf99ae42a7ab1c500ed268456db012014
Ritika Ghosh <ritikaghoshriti@gmail.com>
2024-09-17 03:38:01 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from './Sidebar.jsx'
 import Video from './Video'
-import { useAuth } from '../context/AuthProvider';
+import { useAuth } from '../context/AuthProvider.jsx';
 
 function Home() {
     const { data, loading } = useAuth();
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div className='flex'>
         <Sidebar/>
-    <div >
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
     {!loading &&
             data.map((item) => {
               if (item.type !== "video") return false;
