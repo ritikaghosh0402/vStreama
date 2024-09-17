c73002b74dbdbbb46636ecf14ee3b5fbf7c6c283
Ritika Ghosh <ritikaghoshriti@gmail.com>
2024-09-17 23:15:22 +0530

    Update Video.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,9 @@ function Home() {
     const { data, loading } = useAuth();
     console.log(data);
   return (
-    <div className='flex'>
+    <div className='flex mt-20'>
         <Sidebar/>
+        <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
     {!loading &&
             data.map((item) => {
@@ -17,6 +18,7 @@ function Home() {
               <Video key={item.id} video={item?.video} />
               )
             })}
+    </div>
     </div>
     </div>
   )
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Time from '../loader/Time';
+import { BsFillCheckCircleFill } from "react-icons/bs";
+// import { abbreviateNumber } from "js-abbreviation-number";
 
 function Video({ video }) {
     console.log(video);
@@ -13,6 +15,19 @@ function Video({ video }) {
             <img className="h-full w-full" src={video?.thumbnails[0]?.url} alt="" />
             {video?.lengthSeconds && <Time time={video?.lengthSeconds} />}
         </div>
+        {/* channerl logo & title */}
+        <div className="flex mt-3 space-x-2 ">
+            <div className="flex items-start">
+              <div className="flex h-9 w-9 rounded-full overflow-hidden border">
+                <img
+                  className="h-full w-full rounded-full overflow-hidden"
+                  src={video?.author?.avatar[0].url}
+                  alt=""
+                />
+              </div>
+            </div>
+           
+          </div>
         </div>
         </Link>
         </div>
