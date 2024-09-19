3e5f697ad9f7069803d31332a6db53a963606b7e
Ritika Ghosh <ritikaghoshriti@gmail.com>
2024-09-19 12:57:09 +0530

    Add Channel Name & Other Details on the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className='flex '>
         <Sidebar/>
         <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-5">
     {!loading &&
             data.map((item) => {
               if (item.type !== "video") return false;
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Time from '../loader/Time';
 import { BsFillCheckCircleFill } from "react-icons/bs";
-// import { abbreviateNumber } from "js-abbreviation-number";
+import { abbreviateNumber } from "js-abbreviation-number";
 
 function Video({ video }) {
     console.log(video);
@@ -26,7 +26,27 @@ function Video({ video }) {
                 />
               </div>
             </div>
-           
+            <div>
+              <span className="text-sm font-bold line-clamp-2">
+                {video?.title}
+              </span>
+              <span className="flex items-center font-semibold mt-2 text-[12px] text-gray-600">
+                {video?.author?.title}
+                {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                  <BsFillCheckCircleFill className="text-gray-600 ml-1 text-[12px]" />
+                )}
+              </span>
+              <div className="flex text-gray-500 text-[12px]">
+                <span>{`${abbreviateNumber(
+                  video?.stats?.views,
+                  2
+                )} views`}</span>
+                <span className="flex text-[24px] leading-none font-bold relative top-[-10px] mx-1">
+                  .
+                </span>
+                <span>{video?.publishedTimeText}</span>
+                </div>
+            </div>
           </div>
         </div>
         </Link>
